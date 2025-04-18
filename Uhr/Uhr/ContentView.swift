d1c2158d194f5f20fa3b7ec1340538e34dc38f3b
Café MegaByte <67206705+cafe-megabyte@users.noreply.github.com>
2025-04-18 14:26:34 +0200

    Add files via upload

--- Uhr/Uhr/ContentView.swift
+++ Uhr/Uhr/ContentView.swift
@@ -21,6 +21,7 @@ struct ContentView: View {
                         }
                 )
         }
+            .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 0)
             .frame(minWidth: 300, maxWidth: .infinity, minHeight: 300, maxHeight: .infinity)
             .background(
                 GeometryReader { geometry in
